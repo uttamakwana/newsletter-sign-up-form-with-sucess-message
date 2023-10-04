0a9efe350f1329b344d3c67169883dbc26c55d45
Uttam Makwana <uttamakwana4503@gmail.com>
2023-10-04 21:34:50 +0530

    Adding my all code at once

---
+++ script.js
@@ -0,0 +1,36 @@
+const emailInput = document.querySelector("#card-input-email");
+let emailInputValue;
+const validOrNotSpan = document.querySelector("#valid-or-not");
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const form = document.getElementById("form");
+
+function validEmail(e) {
+  emailInputValue = e.target.value;
+  if (emailInputValue && !emailRegex.test(emailInputValue)) {
+    emailInput.style.border = "1px solid hsl(4, 100%, 67%)";
+    emailInput.style.color = "hsl(4, 100%, 67%)";
+    validOrNotSpan.innerText = "Invalid email❌";
+    validOrNotSpan.style.color = "hsl(4, 100%, 67%)";
+  } else if (emailInputValue && emailRegex.test(emailInputValue)) {
+    emailInput.style.border = "1px solid green";
+    emailInput.style.color = "green";
+    validOrNotSpan.innerText = "Valid✅";
+    validOrNotSpan.style.color = "green";
+  }
+}
+
+emailInput.addEventListener("blur", validEmail);
+
+function submitNewsletter(e) {
+  e.preventDefault();
+  if (!emailInputValue) {
+    emailInput.focus();
+    return alert("Please enter an email address");
+  }
+  if (!emailRegex.test(emailInputValue)) {
+    return alert("Please enter valid email!");
+  }
+  window.location.href = "thankyou.html";
+}
+
+form.addEventListener("submit", submitNewsletter);
